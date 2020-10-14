b8c7ee745f16fff9de9c75c181b8e56fc3290643
Ricardo P <ricardopaucar1363@gmail.com>
2020-10-14 08:35:56 -0500

    set up getters and created enum color and suit

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,28 @@
 public class Card {
     
+
+    private Suit suit;
+
+    private int value;
+
+    private Color color;
+
+    // methods - getters
+
+    public Suit getSuit() {
+        return this.suit
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+    
+
+
+
+
 }
---
+++ src/Suit.java
@@ -0,0 +1,9 @@
+
+
+public enum Suit {
+    SPADES,
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+
+}
